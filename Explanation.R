dd7cdabd5a427c0cf92e1dc4c2e0d5c1d1ef2cc6
Sukhmank5 <123626344+Sukhmank5@users.noreply.github.com>
2025-04-26 23:38:36 -0700

    Delete Explanation.R

--- Explanation.R
+++
@@ -1,9 +0,0 @@
-# ggplot explain:
-# I used a line plot to visualize the S&P 500 index over time, as it effectively shows trends and 
-# patterns in the stock market’s performance across years. This plot highlights periods of growth and decline, 
-# making it ideal for understanding long-term economic changes and investor sentiment.
-
-# To determine the appropriate statistical tests for your SP500 dataset
-# ggplot2 is great for visualizing the results of statistical tests but doesn’t directly perform them.
-# You can use ggplot2 to create scatter plots, histograms, box plots, and regression lines to visualize relationships or distributions, 
-# and then supplement those visuals with the results of statistical tests (like correlation, or t-tests).
